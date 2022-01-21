56b8ee378e715c7a2b5409b186d97b0b1d64da5c
Denivros <45978934+Denivros@users.noreply.github.com>
2022-01-21 18:41:41 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -29,7 +29,6 @@ export default NextAuth({
     CoinbaseProvider({
       clientId: process.env.NEXT_PUBLIC_COINBASE_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_COINBASE_CLIENT_SECRET,
-      options: { redirect_uri: "https://test-denivros.vercel.app/" },
     }),
   ],
   // The secret should be set to a reasonably long random string.
